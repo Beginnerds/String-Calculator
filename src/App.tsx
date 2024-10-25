482cb564c2655d050bbe4dddbd43c28ee7e6d406
Beginnerds <48506689+Beginnerds@users.noreply.github.com>
2024-10-25 17:53:42 +0530

    cleanup App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Calculator from "./components/calculator/Calculator";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="h-screen w-screen bg-slate-800 flex justify-center items-center">
